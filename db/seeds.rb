49887400ab9781ef040747f338f5d087f59b4413
AngieChilds <pirateprincess_avh@yahoo.com>
2018-04-08 12:24:27 +0200

    add account activate

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@ User.create!(name:  "Webmaster",
              email: "pirate.princess.web@gmail.com",
              password:              "pirate@princess",
              password_confirmation: "pirate@princess",
-             status: "admin")
-
+             status: "admin",
+             activated: true,
+             activated_at: Time.zone.now)
+             
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -11,5 +13,7 @@ User.create!(name:  "Webmaster",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
